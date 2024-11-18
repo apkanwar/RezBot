b2ba882cf591f02b27c646c916f72d58b346b59b
Atinderpaul Kanwar <ap_kanwar@outlook.com>
2024-11-18 15:04:19 -0500

    Added requirements.txt file

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # RezBot
+
+### 1. Install all App Requirements
+```pip install -r requirements.txt```
+
+### 2. Setup Discord App Secret Key in ```.env``` File
--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 secret_key = os.getenv("SECRET_KEY")
 
 # Initialize the bot
